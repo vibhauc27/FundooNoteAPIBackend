a8dd70cd6be1085b23eae27bc602a2c1086fdef4
Vibha <vibhauc@gmail.com>
2022-07-29 16:38:14 +0530

    [Vibha]Added:the authorization in swagger

--- CommonLayer/Modal/MSMQmodal.cs
+++ CommonLayer/Modal/MSMQmodal.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Experimental.System.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CommonLayer.Modal
 {
-    internal class MSMQmodal
+    public class MSMQmodal
     {
+        MessageQueue messageQueue = new MessageQueue();
+
+        private void sendData2Queue()
+        {
+            messageQueue.Path = @".\private$\Token";
+
+            messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+            messageQueue.ReceiveCompleted += billingQ _ReceiveCompleted;
+            messageQueue.Send("Desired Messages");
+            messageQueue.BeginReceive();
+            messageQueue.Close();
+        }
+
     }
 }
